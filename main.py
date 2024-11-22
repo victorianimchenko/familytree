07eea900d32f9bf16eadf3ead497311e6f485064
Ren Shevchuk <shevchuk_re@ukr.net>
2024-11-22 22:25:29 +0000

    fixed bugs and errors

--- main.py
+++ main.py
@@ -1,161 +1,31 @@
+from dictionaries import family_tree
 from person import Person, FamilyMember
-
-def get_parents_name(person_info, name):
-    list_ids = FamilyMember(name).find_parents_ids(person_info)
-    names_list = FamilyMember(name).find_parents_name(list_ids)
-    return names_list
-
-def get_children_name(person_info, name):
-    list_ids = FamilyMember(name).find_children(person_info)
-    names_list = FamilyMember(name).find_children_name(list_ids)
-    return names_list
-
-
-def get_siblings(sibling_info, name):
-   siblings_ids = FamilyMember(name).find_siblings(sibling_info)
-   siblings_name = FamilyMember(name).find_sibling_names(siblings_ids)
-   return siblings_name
-
-def get_spouses(person_info, name):
-    spouses_ids = FamilyMember(name).find_spouses_ids(person_info)
-    spouses_name = FamilyMember(name).find_spouses_name(spouses_ids)
-    return spouses_name
-
-
-def print_family_names(name, list_names, type_member):
-   print(f"{name} has {type_member}:")
-   if not list_names:
-      print('Not found')
-   else:
-      for item in list_names:
-         print(item)
-
-
-def immediate_family(person, name):
-   print('IMMEDIATE FAMILY')
-
-   list_parents = get_parents_name(person, name)
-   print_family_names(name, list_parents, "parents")
-
-   list_children = get_children_name(person, name)
-   print_family_names(name, list_children, "children")
-         
-         
-   list_siblings = get_siblings(person, name)
-   print_family_names(name, list_siblings, "siblings")
-   
-   list_spouses = get_spouses(person, name)
-   print_family_names(name, list_spouses, "spouses")
-
-
-
-def find_aunt(name, list_sibling):
-    list = []
-    for parent in list_sibling:
-        person_info = Person(name).find_person(parent)
-
-        sibling_person = get_siblings(person_info, parent)
-        for sibling in sibling_person:
-            list.append(sibling)
-    return list
-
-def find_cousin(name, list_aunts):
-    list_cousin = []
-    
-    for aunt in list_aunts:
-              
-        cousin_info = Person(name).find_person(aunt)
-        cousin_children = get_children_name(cousin_info, aunt)
-        for child in cousin_children:
-            list_cousin.append(child)
-            
-    return list_cousin
-
-def extended_family(person, name):
-    print('EXTENDED FAMILY')
-   
-    list_children = get_children_name(person, name)
-    f_list_children = FamilyMember(name).filtered_alive_member(list_children)
-    print_family_names(name, f_list_children, 'children') 
-
-    list_siblings = get_siblings(person, name)
-    f_list_sibling = FamilyMember(name).filtered_alive_member(list_siblings)
-    print_family_names(name, f_list_sibling, 'siblings')
-    
-
-    list_spouses = get_spouses(person, name)
-    f_list_spouses = FamilyMember(name).filtered_alive_member(list_spouses)
-    print_family_names(name, f_list_spouses, 'spouses')
-
-    list_parents = get_parents_name(person, name)
-    f_list_parents = FamilyMember(name).filtered_alive_member(list_parents)
-    print_family_names(name, f_list_parents, 'parents')
-
-    list_aunt = find_aunt(name, list_parents)
-    f_list_aunt = FamilyMember(name).filtered_alive_member(list_aunt)
-    print_family_names(name, f_list_aunt, 'aunt')
-
-    list_cousin = find_cousin(name, list_aunt)
-    f_list_cousin = FamilyMember(name).filtered_alive_member(list_cousin)
-    print_family_names(name, f_list_cousin, 'cousin')
-
-
-
-
-    
-
-
 
 
 def main():
   
     print("Welcome to the Green Witch Hall!")
-    # name = Person('Baba Calaba', 1990, 1991)
-    # print(name)
-    # x = FamilyMamber('Baba Calaba', 1990).print_member()
+    IMMEDIATE_TYPES= ['spouses', 'children', 'parents', 'sibling']
+    EXTENDED_TYPES = ['spouses', 'children', 'parents', 'sibling', 'aunt', 'cousin']
 
-    # person = input('Enter individual name to show siblings: ')
     name = 'Cornelia Emmersohn'
-    # age = input('Enter individual name to show siblings: ')
-    # Cornelia Emmersohn
 
-# find person by name
-    data = Person(name).find_person(name)
-    
+    tree = FamilyMember(family_tree)
+    data = tree.find_person(name)
+
     print(data, 'Find person')
 
+    # find siblings
+    siblings_name = tree.find_member_list_names(data, 'sibling')
+    tree.print_family_names(name, siblings_name, 'sibling')
 
-# find siblings
-    sibling_list = get_siblings(data, name)
-    print_family_names(name, sibling_list, 'sibling')
-
-# find parents
-    
-    parents_list = get_parents_name(data, name)
-    print_family_names(name, parents_list, 'parents')
-
-
-# find grandchildren    
-    children = get_children_name(data, name)
-    granchildren = None
-    
-    for child in children:
-        child_info = Person(name).find_person(child)
-        granchildren = get_children_name(child_info, child)
-
-    print_family_names(name, granchildren, 'grandchildren')
-        
-# immediate family (parents, children, spouse, siblings)
-    immediate_family(data, name)
-  
-# find extended family (aunt, cousin)
-    extended_family(data, name)
+    #  find grandchildren    
+    grandchildren = tree.get_grandchildren()
 
 
 
-
+    tree.immediate_family(IMMEDIATE_TYPES)
+    tree.extended_family(data, EXTENDED_TYPES)
 
 if __name__ == "__main__":
     main()
-
-    
--- person.py
+++ person.py
@@ -1,126 +1,232 @@
 from dictionaries import family_tree
 
 class Person:
-    def __init__(self, name, birth_year=None, death_year=None):
-        """
-        Ініціалізація об'єкта Person.
+    def __init__(self, name, birth_date=None, death_date=None):
 
-        :param person_id: Унікальний ідентифікатор особи
-        :param name: Ім'я особи
-        :param birth_year: Рік народження
-        :param death_year: Рік смерті (або None, якщо особа ще жива)
-        """
-
+        self.id = ''
         self.name = name
-        self.birth_year = birth_year
-        self.death_year = death_year
+        self.birth_date = birth_date
+        self.death_date = death_date
         self.parents = []  # Список ID батьків
         self.spouses = []  # Список ID партнерів
         self.children = []  # Список ID дітей
-        self.members = family_tree
+        
 
-    def find_id_by_name(self, name):
-        for person_id, details in family_tree.items():
-          if details.get("name") == name:
-            return person_id
-          else:
-              return None
+    def set_info(self, person_info):
+       self.id = person_info.get('id')
+       self.name = person_info.get('name')
+       self.birth_date = person_info.get("birth_date")
+       self.death_date = person_info.get("death_date")
+       self.parents = person_info.get("parents") 
+       self.spouses = person_info.get("spouses")
+       self.children = person_info.get("children")
+
+   #  def find_id_by_name(self, name):
+   #      for person_id, details in family_tree.items():
+   #        if details.get("name") == name:
+   #          return person_id
+   #        else:
+   #            return None
           
-    def find_person_by_id(self, person_id):
+   #  def find_person_by_id(self, person_id):
 
-        for id, details in family_tree.items():
+   #      for id, details in family_tree.items():
 
-           if id == person_id:
-              return {**details}
+   #         if id == person_id:
+   #            return {**details}
     
     
+   #  def find_person(self, name):
+   #      for person_id, details in family_tree.items():
+
+   #        if details.get("name") == name:
+   #            return {'id': person_id, **details}
+          
+    
+    def __str__(self):
+        return f"{self.name} (b. {self.birth_date}" + (f", d. {self.death_date}" if self.death_date else "") + ")"
+
+class FamilyMember(Person):
+    def __init__(self, family_tree):
+        super().__init__(self, family_tree)
+        self.family_tree_data = family_tree
+        self.members = {}
+
+    def find_person_by_name(self, name):
+       for person_id, details in self.family_tree_data.items():
+          if details.get("name") == name:
+              return {'id': person_id, **details}
+       
     def find_person(self, name):
         for person_id, details in family_tree.items():
 
           if details.get("name") == name:
+              self.set_info({'id': person_id, **details})
               return {'id': person_id, **details}
-          
     
-    def __str__(self):
-        return f"{self.name} (b. {self.birth_year}" + (f", d. {self.death_year}" if self.death_year else "") + ")"
-
-class FamilyMember(Person):
-    def __init__(self, name, birth_year=None):
-        super().__init__(name, birth_year)
-
     def find_parents_ids(self, person):
         return person.get('parents')
     
-    def find_parents_name(self, parents_ids):
+    def find_children(self, person):
+       self.children = person.get('children')
+       return person.get('children')
+    
+   #  def find_spouses_ids(self, person):
+   #     return person.get('spouses')
+    
+   #  use UNI FUN
+    def find_list_ids(self, person, type_member):
+       return person.get(f'{type_member}')
+
+    def get_aunt_ids(self, person):
+       list_aunts = []
+       parents = person.get('parents')
+
+       for parent_id in parents:
+
+          parent_info = self.family_tree_data[parent_id]
+          sibling = self.find_siblings(parent_info)
+          sibling.remove(parent_id)
+
+          for sibling_id in sibling:
+             list_aunts.append(sibling_id)
+       return list_aunts
+
+    
+   #  use universal FUNCTION
+    def find_member_list_names(self, person, type_member):
+       list_ids = []
+
+
+       if type_member == 'sibling':
+          list_ids = self.find_siblings(person)
+          list_ids.remove(self.id)
+
+       elif type_member == 'aunt':
+
+          list_ids = self.get_aunt_ids(person)
+
+       elif type_member == 'cousin':
+
+          list_aunt_ids = self.get_aunt_ids(person)
+          list_ids = self.find_cousin(list_aunt_ids)
+          
+       else:
+          list_ids = person.get(f'{type_member}')
+
        name_list = []
-       for id in parents_ids:
-          parent_data = self.find_person_by_id(id)
+       for id in list_ids:
+          parent_data = self.family_tree_data[id]
           parent_name = parent_data.get('name')
           name_list.append(parent_name)
        return name_list
-          
-    
-    def find_children(self, person):
-       return person.get('children')
-    
-    def find_children_name(self, children_ids):
-       name_list = []
-       for id in children_ids:
-          children_data = self.find_person_by_id(id)
-          children_name = children_data.get('name')
-          name_list.append(children_name)
-       return name_list
-       
-    def find_siblings(self, person):
-       siblings = set()
-
-       parents_ids = self.find_parents_ids(person)
-
-       for id in parents_ids:
-         parent_info = self.find_person_by_id(id)
-         children = self.find_children(parent_info)
-         siblings.update(children)
-       
-       siblings.discard(person["id"])
-
-       return siblings
-    
-    def find_sibling_names(self, sibling_ids):
-       list_names = []
-       for id in sibling_ids:
-         sibling_data = self.find_person_by_id(id)
-         sibling_name = sibling_data.get('name')
-         list_names.append(sibling_name)
-       return list_names
-    
-
-    def find_spouses_ids(self, person):
-       return person.get('spouses')
    
-    def find_spouses_name(self, ids):
-      list_spouses_name = []
-      for id in ids:
-        spouses_data = self.find_person_by_id(id)
-        spouses_name = spouses_data.get('name')
-        list_spouses_name.append(spouses_name)
-      return list_spouses_name
-    
+
     def filtered_alive_member(self, list_names):
       list = []
       for name in list_names:
-         data = self.find_person(name)
+         data = self.find_person_by_name(name)
          death_date = data.get("death_date")
          if death_date is None:
             list.append(name)
       return list
                    
 
-          
+    def find_siblings(self, person):
+      siblings = []  # Змінюємо на список
 
 
-       
-        
+    # Отримуємо список ідентифікаторів батьків
+      parents_ids = person.get('parents', []) if person else self.parents
+
+     
+    # Перевіряємо, чи є батьки
+      if not parents_ids:
+          print('No parents found.')
+          return siblings  # Повертаємо порожній список, якщо батьків немає
     
+    # Проходимо по всіх батьках
+      for parent_id in parents_ids:
+          parent_info = family_tree.get(parent_id)
+        
+          if parent_info:
+            # Знаходимо дітей для кожного з батьків
+            children = self.find_children(parent_info)
+            siblings.extend(children)  # Додаємо знайдених дітей до списку
+          else:
+             print(f'Parent ID {parent_id} not found in family_tree.')
+    
+    # Видаляємо ідентифікатор самої особи, якщо він є
+      if self.id in siblings:
+          siblings.remove(self.id)
+      
+
+    
+    # Уникнення дублікатів, якщо це потрібно
+      siblings = list(set(siblings))
+    
+      return siblings
+
+    def find_aunt(self, list_sibling):
+      list = []
+
+      for parent in list_sibling:
+        person_info = self.find_person(parent)
+
+        sibling_person = self.find_siblings(person_info)
+
+        for sibling in sibling_person:
+            list.append(sibling)
+      return list  
+    
+    def find_cousin(self, list_aunts):
+      list_cousin = []
+
+      for aunt in list_aunts:
+        cousin_info = self.family_tree_data[aunt]
+        cousin_children = cousin_info.get('children')
+
+        for child in cousin_children:
+            list_cousin.append(child)
+            
+      return list_cousin
+    
+    def get_grandchildren(self):
+           children = self.children
+           granchildren = None
+    
+           for child in children:
+             child_info = self.family_tree_data[child]
+             granchildren = self.find_member_list_names(child_info, 'children')
+
+           self.print_family_names(self.name, granchildren, 'grandchilren')
+           return granchildren
+    
+    def immediate_family(self, member_types):
+       print('IMMEDIATE FAMILY')
+
+       for member in member_types:
+         list = self.find_member_list_names(self.family_tree_data[self.id], member)
+         self.print_family_names(self.name, list, member)
+
+    def extended_family(self, person, member_types):
+      print('EXTENDED FAMILY')
+     
+      for member in member_types:
+   
+         list = self.find_member_list_names(person, member)
+         filtered_list = self.filtered_alive_member(list)
+         self.print_family_names(self.name, filtered_list, member)
+
+    
+    def print_family_names(self, name, list_names, type_member):
+      print(f"{name} has {type_member}:")
+      if not list_names:
+        print('Not found')
+      else:
+        for item in list_names:
+           print(item)
+           
     def print_member(self):
         print(self.name)
 
